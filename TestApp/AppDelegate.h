398854d257cd3f066766e5c9b23ad5df43716d95
STISUR <STIAN_S_@HOTMAIL.COM>
2014-07-16 22:28:49 +0200

    Testing to commit from OSX
    Signed-off-by: STISUR <STIAN_S_@HOTMAIL.COM>

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Stian Surén on 16/07/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Stian Surén on 16/07/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
